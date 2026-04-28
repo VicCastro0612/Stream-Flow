d7b8c2a88c88272c1a2f36334ef9aa078d251070
ClaudioFigueroa15 <cl.figueroac@duocuc.cl>
2026-04-27 20:45:22 -0400

    Proyecto Inicial con Dependencias
    Se integra el proyecto base generado desde Spring Initializr con las dependencias necesarias para el funcionamiento.

---
+++ src/main/java/evaluacion2/streamflow/StreamflowApplication.java
@@ -0,0 +1,13 @@
+package evaluacion2.streamflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evaluacion2/streamflow/StreamflowApplicationTests.java
@@ -0,0 +1,13 @@
+package evaluacion2.streamflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
